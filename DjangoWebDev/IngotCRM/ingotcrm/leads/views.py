8411cc01eb71865ff737ad9535af21331766deb3
Narokan <nick.kirichenko.dev@gmail.com>
2023-01-05 14:35:04 +0200

    url and templates changes
    url is now saving names in templates which is more cool; templates are using blocks for DRY principle.

--- DjangoWebDev/IngotCRM/ingotcrm/leads/views.py
+++ DjangoWebDev/IngotCRM/ingotcrm/leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 
 def lead_list(request):
@@ -16,34 +16,106 @@ def lead_list(request):
 
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
+
     context = {
         'lead': lead
     }
+
     return render(request, "lead_details.html", context)
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
 
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
 
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
-
+            form.save()
             return redirect("/leads")
 
     context = {
         'form': form
     }
+
     return render(request, "lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    
+    context = {
+        'form': form,
+        'lead': lead
+    }
+
+    return render(request, "lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+
+    return redirect("/leads")
+
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+
+#     if request.method == 'POST':
+#         form = LeadForm(request.POST)
+
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+
+#             lead.first_name=first_name
+#             lead.last_name=last_name
+#             lead.age=age
+#             lead.save()
+            
+#             return redirect('/leads')
+
+#     context = {
+#         'form': form,
+#         'lead': lead
+#     }
+
+#     return render(request, "lead_update.html", context)
+
+
+# def lead_create(request):
+#     form = LeadModelForm()
+
+#     if request.method == "POST":
+#         form = LeadModelForm(request.POST)
+
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             agent = Agent.objects.first()
+            
+#             Lead.objects.create(
+#                 first_name=first_name,
+#                 last_name=last_name,
+#                 age=age,
+#                 agent=agent
+#             )
+
+#             return redirect("/leads")
+
+#     context = {
+#         'form': form
+#     }
+
+#     return render(request, "lead_create.html", context)
